103a1e1f28306c6846c4ef598fda6291151616ad
dockhands <danhawkins4@gmail.com>
2017-04-05 00:49:52 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key:'e3sCFUotcZRy9yUuCjFQmwLNn',
-  consumer_secret:'AEb5IQfmvtBYf4pur9CMlOsSU1QazNcfcFq65kOPRdc7zdgDag',
-  access_token:'837553105994571778-pE31BaCHZlVYCwiAftoUl0m0o5M7wXs',
-  access_token_secret:'HyPYqcIn8pekGRk42qB80e4JOKdQsshA47kd0gvw8kSyl'
+  consumer_key:'de3sCFUotcZRy9yUuCjFQmwLNnh',
+  consumer_secret:'dAEb5IQfmvtBYf4pur9CMlOsSU1QazNcfcFq65kOPRdc7zdgDagh',
+  access_token:'d837553105994571778-pE31BaCHZlVYCwiAftoUl0m0o5M7wXsh',
+  access_token_secret:'dHyPYqcIn8pekGRk42qB80e4JOKdQsshA47kd0gvw8kSylh'
 }
